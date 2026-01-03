2beebcd4c6469ef410708b1335b9c7da6c211fd9
Retro52 <orlenko.anto@gmail.com>
2026-01-03 20:07:33 +0100

    bit of punctuality

--- src/bytes.hpp
+++ src/bytes.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <types.hpp>
 
 #include <Tracy/Tracy.hpp>
@@ -8,71 +9,77 @@
 struct bytes
 {
 public:
-    explicit bytes(u64 size)
+    explicit bytes(const u64 size)
         : m_size(size)
         , m_memory(std::make_unique<u8[]>(size))
     {
     }
 
-    bytes(u64 size, const void* data)
+    bytes(const u64 size, const void* data)
         : bytes(size)
     {
         ZoneScoped;
         std::copy_n(static_cast<const u8*>(data), size, m_memory.get());
     }
 
-    void* data() noexcept
+    [[nodiscard]] void* data() noexcept
     {
         return m_memory.get();
     }
 
-    const void* data() const noexcept
+    [[nodiscard]] const void* data() const noexcept
     {
         return m_memory.get();
     }
 
-    const void* begin() const noexcept
+    [[nodiscard]] const void* begin() const noexcept
     {
         return m_memory.get();
     }
 
-    const void* end() const noexcept
+    [[nodiscard]] const void* end() const noexcept
     {
         return m_memory.get() + m_size;
     }
 
     template<typename T>
-    T* get() noexcept
+    [[nodiscard]] T* get() noexcept
         requires(std::is_integral_v<T> || std::is_void_v<T>)
     {
         return reinterpret_cast<T*>(m_memory.get());
     }
 
     template<typename T>
-    const T* get() const noexcept
+    [[nodiscard]] const T* get() const noexcept
         requires(std::is_integral_v<T> || std::is_void_v<T>)
     {
         return reinterpret_cast<T*>(m_memory.get());
     }
 
     template<typename T>
-    T& operator[](const u64 index) noexcept
+    [[nodiscard]] T& operator[](const u64 index) noexcept
     {
         return get<T>()[index];
     }
 
     template<typename T>
-    const T& operator[](const u64 index) const noexcept
+    [[nodiscard]] const T& operator[](const u64 index) const noexcept
     {
         return get<T>()[index];
     }
 
-    u64 size() const noexcept
+    [[nodiscard]] u64 size() const noexcept
     {
         return m_size;
     }
 
-    bool empty() const noexcept
+    template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+    [[nodiscard]] u64 length() const noexcept
+    {
+        return size() / sizeof(T);
+    }
+
+    [[nodiscard]] bool empty() const noexcept
     {
         return m_size == 0;
     }
@@ -80,12 +87,6 @@ public:
     operator bool() const noexcept
     {
         return !empty();
-    }
-
-    template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-    u64 length() const noexcept
-    {
-        return size() / sizeof(T);
     }
 
 private:
