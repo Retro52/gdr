887c00386b7285dc9b5060ee19890a9f1b34547f
Anton Orlenko <hello.retro52@gmail.com>
2026-09-11 10:18:40 +0200

    remade project structure a bit to separate 'common' stuff into a standalone lib

--- gdrlib/src/render/platform/vk/vk_device.hpp
+++ gdrlib/src/render/platform/vk/vk_device.hpp
@@ -86,7 +86,7 @@ namespace render
     {
         const char* app_name = "";
         u32 app_version      = 0;
-        u32 device_id_hint   = -1;
+        u32 device_id_hint   = static_cast<u32>(-1);
         rendering_features_table device_features;
     };
 
